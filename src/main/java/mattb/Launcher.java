ce8c5b19447dc2eb2a5bc7f91d9337465848e78b
Matthew Beicke <mbeicke@gi.gcsnet.biz>
2026-04-15 11:14:47 -0400

    GUI now asks for name (like CLI does) and opens up to a page to view chat

---
+++ src/main/java/mattb/Launcher.java
@@ -0,0 +1,9 @@
+package mattb;
+
+import mattb.client.ClientGUI;
+
+public class Launcher {
+    static void main(String[] args) {
+        ClientGUI.main(args);
+    }
+}
--- src/main/java/mattb/client/ClientGUI.java
+++ src/main/java/mattb/client/ClientGUI.java
@@ -1,21 +1,46 @@
 package mattb.client;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ClientGUI extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
+    public void start(Stage stage) {
+        stage.setTitle("Chat");
 
-        Scene scene = new Scene(root, 400, 300);
+        Label history = new Label();
+        history.setWrapText(true);
+        TextField input = new TextField();
+        Label welcome = new Label();
+        VBox bottom = new VBox(2);
+        bottom.getChildren().addAll(new Label("  Enter messages below (press enter to send):"), input);
+        BorderPane border = new BorderPane(history, welcome, null, bottom, null);
+        BorderPane.setAlignment(welcome, Pos.CENTER);
+        Scene chat = new Scene(border, 500, 500);
 
-        primaryStage.setTitle("javafx");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        TextField name = new TextField();
+        name.setOnAction(event -> {
+            if (!name.getText().isBlank()) {
+                // do the client stuff here
+                welcome.setText("Welcome " + name.getText());
+                stage.setScene(chat);
+            }
+        });
+        Label nameLabel = new Label("Enter your name (what you want to be displayed as) below:");
+        VBox root = new VBox(nameLabel, name);
+        root.setAlignment(Pos.CENTER);
+
+        Scene nameScene = new Scene(root, 400, 300);
+
+        stage.setScene(nameScene);
+        stage.show();
     }
 
     public static void main(String[] args) {
