bc1eaa30c712a808fdf0e73abe865a0803f9f559
Matthew Beicke <mbeicke@gi.gcsnet.biz>
2026-04-15 09:16:29 -0400

    Added main files for the client and server

---
+++ src/main/java/mattb/client/Client.java
@@ -0,0 +1,7 @@
+package mattb.client;
+
+public class Client {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/main/java/mattb/server/Server.java
@@ -0,0 +1,7 @@
+package mattb.server;
+
+public class Server {
+    public static void main(String args[]) {
+
+    }
+}
